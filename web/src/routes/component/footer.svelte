e4c5b13184f01fd3020d57d8817977f33c8470d4
kaviyarasancmk02 <cmkkaviyarasanmm@gmail.com>
2024-10-21 01:35:07 +0530

    Added details to footer

--- web/src/routes/component/footer.svelte
+++ web/src/routes/component/footer.svelte
@@ -1,23 +1,97 @@
-<footer class="bg-gray-800 text-white py-6">
-  <div class="container mx-auto text-center">
-    <p class="mb-4">
-      Contact: <a
-        href="mailto:your-email@example.com"
-        class="underline hover:text-gray-400">your-email@example.com</a
-      > | Phone: +91-XXXXXXXXXX
-    </p>
-    <div class="social-links space-x-4">
-      <a
-        href="https://linkedin.com/in/your-profile"
-        target="_blank"
-        class="hover:text-gray-400 transition duration-300">LinkedIn</a
-      >
-      <a
-        href="https://github.com/your-profile"
-        target="_blank"
-        class="hover:text-gray-400 transition duration-300">GitHub</a
-      >
-      <!-- Add more social icons as needed -->
+<script>
+  import {userDetails} from "../../data/userDetails.js";
+  console.log(typeof userDetails);
+  const verticalLine =
+    '<div class="hidden md:block rounded w-0.5 h-32 bg-gray-100 opacity-10"></div>';
+  const horizontalLine =
+    '<div class="hidden md:block rounded w-2/3 h-0.5 bg-gray-100 opacity-10"></div>';
+
+  const socialMedia = {
+    linkedin: {
+      name: "LinkedIn",
+      url: userDetails.linkedin,
+    },
+    github: {
+      name: "HitHub",
+      url: userDetails.github,
+    },
+    instagram: {
+      name: "Instagram",
+      url: userDetails.instagram,
+    },
+    facebook: {
+      name: "FaceBook",
+      url: userDetails.facebook,
+    },
+    youtube: {
+      name: "YouTube",
+      url: userDetails.youtube,
+    },
+  };
+</script>
+
+<footer
+  class="bg-indigo-950 text-white p-6 flex flex-col items-center justify-between gap-4"
+>
+  <div
+    class="w-full flex flex-col md:flex-row items-start md:items-center justify-between gap-4"
+  >
+    <div class="">
+      <span class="font-semibold text-lg text-gray-100"> Contact: </span>
+      <ul class="text-sm p-2 space-y-1 text-gray-200">
+        <li class="">Address: {userDetails.simpleAddress}</li>
+        <li class="">Phone: {userDetails.phone}</li>
+        <li class="">Whatsapp: {userDetails.whatsapp}</li>
+        <li class="">Email: {userDetails.email}</li>
+      </ul>
     </div>
+    {@html verticalLine}
+    <div class="">
+      <span class="font-semibold text-lg text-gray-100"> Education </span>
+      <ul class="text-sm p-2 space-y-1 text-gray-200">
+        <li class="">
+          {userDetails.education.msc.name} - {userDetails.education.msc
+            .percentage}
+        </li>
+        <li class="">
+          {userDetails.education.bsc.name} - {userDetails.education.bsc
+            .percentage}
+        </li>
+        <li class="">
+          {userDetails.education.hsc.name} - {userDetails.education.hsc
+            .percentage}
+        </li>
+        <li class="">
+          {userDetails.education.sslc.name} - {userDetails.education.sslc
+            .percentage}
+        </li>
+      </ul>
+    </div>
+    {@html verticalLine}
+    <div class="">
+      <span class="font-semibold text-lg text-gray-100">
+        Personal Details
+      </span>
+      <ul class="text-sm p-2 space-y-1 text-gray-200">
+        <li class="">Name: {userDetails.fullName}</li>
+        <li class="">Date of Birth: {userDetails.dateOfBirth}</li>
+        <li class="">Marital Status: {userDetails.maritalStatus}</li>
+        <li class="">Languages: {userDetails.languages}</li>
+      </ul>
+    </div>
+  </div>
+  {@html horizontalLine}
+  <div class="flex items-center justify-center gap-6 flex-wrap p-4">
+    {#each Object.entries(socialMedia) as [key, link]}
+      <a
+        href={link.url}
+        target="_blank"
+        class="relative group transition-all duration-300 ease-in-out"
+        >{link.name}
+        <span
+          class="absolute bottom-0 left-0 h-0.5 rounded bg-gray-300 transition-all duration-300 ease-in-out w-0 group-hover:w-full"
+        ></span>
+      </a>
+    {/each}
   </div>
 </footer>
--- web/src/routes/component/hero.svelte
+++ web/src/routes/component/hero.svelte
@@ -1,5 +1,6 @@
 <script>
   import kaviyarsanStylish from "../../assets/images/kaviyarsanStylish.jpg";
+  import Ping from "../animation/ping.svelte";
 
   let greetingName = "Hello, I'm KAVIYARASAN";
   const verticalLine =
@@ -9,8 +10,9 @@
   const mySkills = [
     "HTML",
     ["CSS", "Tailwind CSS"],
-    ["JavaScript", ["SvelteJS"]],
+    ["JavaScript", "SvelteJS"],
     "Git &GitHub",
+    ["MS Power Platform", "Power Apps, Power Automate, Power Virtual Agent"],
   ];
 </script>
 
@@ -19,13 +21,13 @@
   class="md:min-h-[90vh] bg-white text-gray-800 transition-all duration-300 ease-in-out"
 >
   <div
-    class="p-4 md:p-8 lg:p-12 flex flex-col md:flex-row items-center justify-stretch gap-6"
+    class="p-4 md:p-6 lg:p-10 flex flex-col md:flex-row items-center justify-stretch gap-4"
   >
     <div
       class="intro text-center flex flex-col items-center justify-center gap-4 transition-all duration-500 ease-in-out"
     >
       <div
-        class="flex flex-col md:flex-row items-center md:items-start justify-center gap-4 md:gap-6 lg:gap-10"
+        class="flex flex-col md:flex-row items-center md:items-start justify-center gap-4 md:gap-4 lg:gap-10"
       >
         <div>
           <div
@@ -52,45 +54,56 @@
           </h1>
         </div>
         <div
-          class="md:w-1/4 min-w-40 border border-stone-400 p-2 md:p-4 rounded-md text-pretty text-base font-semibold text-slate-600 transition-all duration-500 ease-in-out text-justify relative"
+          class="md:w-1/4 m-2 min-w-40 border border-green-400 p-2 md:p-4 rounded-md text-pretty text-base font-semibold text-green-600 transition-all duration-500 ease-in-out text-justify relative"
         >
           Eager and motivated fresher seeking Entry-Level web developer
           opportunities.
-          <div
-            class="w-2 h-2 absolute top-1 right-1 bg-blue-600 rounded-full transition-all duration-500"
-          ></div>
-          <div
-            class="w-2 h-2 absolute top-1 right-1 bg-blue-600 rounded-full animate-ping transition-all duration-500"
-          ></div>
+          <div class="absolute top-1 right-1">
+            <Ping color="bg-green-600" />
+          </div>
         </div>
       </div>
 
       <p class="text-justify p-4">
-        Driven and enthusiastic computer science graduate seeking entry-level
-        opportunities to begin my career. I am passionate about learning and
-        eager to contribute to a collaborative and dynamic team. Ready to apply
-        my skills, grow professionally, and take on new challenges in a
-        fast-paced environment
+        Driven and enthusiastic computer science graduate seeking
+        <strong> Entry-Level </strong>
+        opportunities to begin my career. I am passionate about learning and eager
+        to contribute to a collaborative and dynamic team. Ready to apply my skills,
+        grow professionally, and take on new challenges in a fast-paced environment.
       </p>
     </div>
 
     {@html verticalLine}
 
-    <div class="p-4 md:w-1/4 flex flex-col items-start w-full gap-4">
+    <div class="p-4 flex flex-col items-start gap-4">
       <h2 class="text-3xl font-semibold mb-6">My Skills</h2>
-      <ul class="pl-6 flex items-start gap-6 flex-wrap">
+      <ul class="pl-6 flex items-start justify-between gap-6 flex-wrap">
         {#each mySkills as skill}
-          <button
-            class="min-w-max w-28 rounded border border-b-2 border-b-indigo-500 hover:border-indigo-500 py-1 px-4 hover:bg-indigo-100 transition-all duration-300 ease-in-out"
+          <span
+            class="transition-all duration-300 ease-in-out flex items-center justify-start gap-4"
           >
-            <li class="text-indigo-500 hover:text-indigo-600 font-semibold">
-              {#if typeof skill === "string"}
+            <Ping />
+            {#if typeof skill === "string"}
+              <span
+                class="underline decoration-indigo-500 decoration-wavy text-indigo-500 -indigo-600 font-semibold"
+              >
                 {skill}
-              {:else if typeof skill === "object"}
-                {skill[0]}
-              {/if}
-            </li>
-          </button>
+              </span>
+            {:else if typeof skill === "object"}
+              <div>
+                <div
+                  class="underline text-ellipsis decoration-indigo-500 decoration-wavy text-indigo-500 -indigo-600 font-semibold"
+                >
+                  {skill[0]}
+                </div>
+                <div
+                  class=" text-wrap text-gray-500 font-medium text-sm text-ellipsis"
+                >
+                  ({skill[1].slice(0)})
+                </div>
+              </div>
+            {/if}
+          </span>
         {/each}
       </ul>
     </div>
